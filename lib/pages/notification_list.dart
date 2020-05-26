206f984cd871d2469dfc8b8e6bea7fae4367831b
fabiomontello <montellofabio@gmail.com>
2020-05-26 13:59:34 +0200

    Update notification_list.dart

--- lib/pages/notification_list.dart
+++ lib/pages/notification_list.dart
@@ -217,7 +217,7 @@ class NotificationList extends StatelessWidget {
                 notifListNext.length == 0
                     ? ListTile(
                         title: Text(
-                          'Nessuna notifica presente',
+                          'Non hai ancora inserito nessun appuntamento',
                           style: TextStyle(
                             fontSize: 18,
                             fontFamily: 'Ubuntu',
@@ -280,7 +280,7 @@ class NotificationList extends StatelessWidget {
                 notifListBef.length == 0
                     ? ListTile(
                         title: Text(
-                          'Nessuna notifica presente',
+                          'Non hai ancora inserito nessun appuntamento',
                           style: TextStyle(
                             fontSize: 18,
                             fontFamily: 'Ubuntu',
